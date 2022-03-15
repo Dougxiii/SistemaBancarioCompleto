05dfbf8c678defa65d8e75904509b7188acfb44b
Douglas Barcellos <douglasbt.dev@gmail.com>
2022-03-15 18:26:01 -0300

    Initial commit

---
+++ src/br/com/fiap/banco/Conta.java
@@ -0,0 +1,35 @@
+package br.com.fiap.banco;
+
+public class Conta {
+
+    int agencia;
+    int numero;
+    double saldo;
+
+    public Conta(){ // CONSTRUTOR PADRÃO
+
+    }
+
+    public Conta(int agencia, int numero, double saldo) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.saldo = saldo;
+    }
+
+    public void depositar(double valor){ //deposita valor ao saldo
+
+        this.saldo += valor;
+    }
+
+    public void retirar(double valor){ //saca o valor do saldo
+
+        this.saldo -= valor;
+    }
+
+    public double verificarSaldo(){ //mostra o valor atual do saldo
+        return  this.saldo;
+    }
+
+
+
+}
---
+++ src/br/com/fiap/banco/Teste.java
@@ -0,0 +1,44 @@
+package br.com.fiap.banco;
+
+import java.util.Scanner;
+
+public class Teste {
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        Conta cc = new Conta();
+        cc.saldo = 50.0;
+        cc.agencia = 123;
+        cc.numero = 987;
+
+        cc.depositar(1000);
+
+        System.out.println(cc.verificarSaldo());
+
+        Conta contaPoupanca = new Conta(111,222,1000);
+
+        contaPoupanca.depositar(650.99);
+        System.out.println(contaPoupanca.verificarSaldo());
+
+        contaPoupanca.retirar(50.99);
+        System.out.println(contaPoupanca.verificarSaldo());
+
+        Conta cc2 = new Conta();
+        System.out.println("Agencia: ");
+        cc2.agencia = leitor.nextInt()
+        ;
+        System.out.println("Numero: ");
+        cc2.numero = leitor.nextInt();
+
+        cc2.depositar(500);
+        System.out.println("agencia: " + cc2.agencia);
+        System.out.println("numero: " + cc2.numero);
+        System.out.println("saldo: " + cc2.verificarSaldo());
+
+
+
+
+
+    }
+}
