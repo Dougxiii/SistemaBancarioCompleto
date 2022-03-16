fd7e71e5cbd9398d08adab507db252e1949dd820
Douglas Barcellos <84218639+Dougxiii@users.noreply.github.com>
2022-03-16 07:06:39 -0300

    javabeans
    implementando java beans, entrada de dados falhou

--- src/br/com/fiap/banco/Conta.java
+++ src/br/com/fiap/banco/Conta.java
@@ -1,10 +1,13 @@
 package br.com.fiap.banco;
 
-public class Conta {
+import java.io.Serializable;
+
+public class Conta implements Serializable {
+
     // Atributos de classe
-    int agencia;
-    int numero;
-    double saldo;
+    private int agencia;
+    private int numero;
+    private double saldo;
 
     public Conta(){ // CONSTRUTOR PADRÃO
 
@@ -14,6 +17,22 @@ public class Conta {
         this.agencia = agencia;
         this.numero = numero;
         this.saldo = saldo;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
     }
 
     public void depositar(double valor){ //deposita valor ao saldo
@@ -26,7 +45,7 @@ public class Conta {
         this.saldo -= valor;
     }
 
-    public double verificarSaldo(){ //mostra o valor atual do saldo
+    public double getSaldo(){ //mostra o valor atual do saldo
         return  this.saldo;
     }
 
--- src/br/com/fiap/banco/Teste.java
+++ src/br/com/fiap/banco/Teste.java
@@ -8,35 +8,36 @@ public class Teste {
         Scanner leitor = new Scanner(System.in);
 
         Conta cc = new Conta();
-        cc.saldo = 50.0;
-        cc.agencia = 123;
-        cc.numero = 987;
+        cc.depositar(50.89);
+        cc.setAgencia(123);
+        cc.setNumero(987);
 
         cc.depositar(1000);
 
-        System.out.println(cc.verificarSaldo());
+        System.out.println(cc.getSaldo());
 
         Conta contaPoupanca = new Conta(111,222,1000);
 
         contaPoupanca.depositar(650.99);
-        System.out.println(contaPoupanca.verificarSaldo());
+        System.out.println(contaPoupanca.getSaldo());
 
         contaPoupanca.retirar(50.99);
-        System.out.println(contaPoupanca.verificarSaldo());
+        System.out.println(contaPoupanca.getSaldo());
 
+        /*
         //TESTANDO ENTRADA DE DADOS
 
         Conta cc2 = new Conta();
         System.out.println("Agencia: ");
-        cc2.agencia = leitor.nextInt();
+        cc2.getAgencia() = leitor.nextInt();
         System.out.println("Numero: ");
-        cc2.numero = leitor.nextInt();
+        cc2.setNumero() = leitor.nextInt();
 
         cc2.depositar(500);
-        System.out.println("agencia: " + cc2.agencia);
-        System.out.println("numero: " + cc2.numero);
-        System.out.println("saldo: " + cc2.verificarSaldo());
-
+        System.out.println("agencia: " + cc2.getAgencia());
+        System.out.println("numero: " + cc2.getNumero());
+        System.out.println("saldo: " + cc2.getSaldo());
+        */
 
 
 
